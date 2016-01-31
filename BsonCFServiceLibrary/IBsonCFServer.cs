d9088a9e9b1322e75300748150655b3923afcd58
samaelic <samaelic@yeah.net>
2016-02-01 00:22:11 +0800

    BsonCF库可用

--- BsonCFServiceLibrary/IBsonCFServer.cs
+++ BsonCFServiceLibrary/IBsonCFServer.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.ServiceModel.Web;
 using System.Text;
 
-namespace BsonCFServicer
+namespace BsonCFServiceLibrary
 {
     // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IService1”。
     [ServiceContract]
-    public interface IService1
+    public interface IBsonCFServer
     {
-
         [OperationContract]
         string GetData(int value);
 
@@ -22,8 +20,8 @@ namespace BsonCFServicer
         // TODO: 在此添加您的服务操作
     }
 
-
     // 使用下面示例中说明的数据约定将复合类型添加到服务操作。
+    // 可以将 XSD 文件添加到项目中。在生成项目后，可以通过命名空间“BsonCFServiceLibrary.ContractType”直接使用其中定义的数据类型。
     [DataContract]
     public class CompositeType
     {
